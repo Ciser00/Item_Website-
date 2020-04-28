b1c6b7c479faf1a2fb2a7f9de3358e18cdedde0c
Ciser00 <47250680+Ciser00@users.noreply.github.com>
2020-04-28 14:20:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var hide = document.getElementById('disapear');
+
+document.getElementById("lbtn").addEventListener("click", test());
+document.getElementById("rbtn").addEventListener("click", green());
+//document.getElementById("shh").addEventListener("click", shh());
+//document.getElementById("disapear").addEventListener("click", leave());
+
+
+function test(){
+
+  document.body.style.backgroundColor="red";
+
+}
+function green(){
+  document.body.style.backgroundColor="#006699";
+
+
+}
+//function fire(){
+//  document.getElementById("firebox").style.visibility = "visable";
+//}
